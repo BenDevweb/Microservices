4e02f6093414cdbb286807898838f16d445119b8
BenDevweb <btechinnov746@hmail.com>
2026-04-23 20:41:38 +0100

    Creation du fichier api avec axios pour le fetch d'API

--- React/finance-app/src/App.js
+++ React/finance-app/src/App.js
@@ -1,2 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import api from './api';
+
+const App = () => {
+  const [transactions, settransactions] = useState([]);
+  const [formData, setFormData]  = useState({
+      amount: '',
+      category: '',
+      description: '',
+      is_income: false,
+      date: ''
+});
+
+const fetchTransactions = async () => {
+  const res = await api.get('/transactions/');
+  settransactions(res.data)
+};
+
+useEffect(() => {
+  fetchTransactions();
+
+}, [])
+
+const handleInputChange = (event) => {
+  const value = event.target === 'checkbox' ? event.target.checked : event.target.value;
+  setFormData({
+    ...formData,
+    (event)
+  })
+}
+
+
+}
 
 export default App;
